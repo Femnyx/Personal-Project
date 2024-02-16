2d4fc313e1e3c8ec260c3f3e1962fb2006c81360
Zander <Zandermcvey@gmail.com>
2024-02-16 12:43:44 -0600

    Mobile Controls | Taco Libre

--- Assets/StarterAssets/Mobile/Scripts/Utilities/MobileDisableAutoSwitchControls.cs
+++ Assets/StarterAssets/Mobile/Scripts/Utilities/MobileDisableAutoSwitchControls.cs
@@ -8,14 +8,13 @@ For the time-being; this script will disable a PlayerInput's auto switch control
 */
 
 using UnityEngine;
-#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
-#endif
+
 
 public class MobileDisableAutoSwitchControls : MonoBehaviour
 {
-    
-#if ENABLE_INPUT_SYSTEM && (UNITY_IOS || UNITY_ANDROID)
+
+#if (UNITY_IOS || UNITY_ANDROID)
 
     [Header("Target")]
     public PlayerInput playerInput;
@@ -30,6 +29,12 @@ public class MobileDisableAutoSwitchControls : MonoBehaviour
         playerInput.neverAutoSwitchControlSchemes = true;
     }
 
+#else
+
+    private void Awake()
+    {
+        Destroy(this.gameObject);
+    }
 #endif
-    
+
 }
